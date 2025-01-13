6fd6ad7a3c4787f7d8569d3e403eed28075083a5
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2025-01-13 22:14:04 +0600

    Time: 14 ms (69.09%), Space: 12.4 MB (41.82%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -5,18 +5,18 @@ class Solution(object):
         p = len(word1)
         q = len(word2)
         
-        # Alternate between characters of word1 and word2
+         
         while i < p or j < q:
-            if i < p:  # Add from word1 if there are remaining characters
+            if i < p:  
                 list1.append(word1[i])
                 i += 1
-            if j < q:  # Add from word2 if there are remaining characters
+            if j < q: 
                 list1.append(word2[j])
                 j += 1
         
-        return ''.join(list1)  # Convert list to a string
+        return ''.join(list1) 
 
-# Test the function
+
 word1 = "abc"
 word2 = "pqr"
 obj = Solution()
