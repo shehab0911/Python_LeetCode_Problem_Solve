4bb11ecd6d959c02021d407f8352b5ee50c5f94a
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2025-01-16 23:15:47 +0600

    Time: 0 ms (100%), Space: 12.7 MB (6.47%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -1,10 +1,41 @@
 class Solution(object):
     def applyOperations(self, nums):
-        n=len(nums)
-        j=1
-        for i in range(1,n):
-            if nums[i]==[i+1]:
-                nums[j]=nums[i]+nums[i-1]
+        n = len(nums)
+        i=0
+        j = 1
+        k=1
+        while i<n and j<n:
+            if nums[i]==nums[j]:
+                nums[i]=nums[i]+nums[j]
+                nums[j] = 0
+                i+=1
                 j+=1
-        return nums[j]
-        
+            else:
+                i+=1
+                j+=1
+
+        list2=[]
+        list1=[]
+        for i in nums:
+            if i!=0:
+                list2.append(i)
+            else:
+                list1.append(i)
+
+
+        return list2+list1
+
+
+
+
+
+
+
+
+
+nums = [1,2,2,1,1,0]
+
+obj=Solution()
+result=obj.applyOperations(nums)
+print(result)
+
