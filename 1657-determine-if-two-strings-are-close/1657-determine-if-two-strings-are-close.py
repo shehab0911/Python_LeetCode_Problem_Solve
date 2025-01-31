c85bc4f546da129a4ae3ddfc9a758ace0d133300
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2025-01-31 23:54:56 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def closeStrings(self, word1, word2):
+        if len(word1)==len(word2):
+            return True
+        else:
+            return False
+        
+        
