5228bff4bfca520e3aafcb409334cbfe9fd22bb7
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2025-03-16 01:39:32 +0600

    Time: 0 ms (100%), Space: 13.4 MB (28.4%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        totalSum=sum(nums)
+        prevSum=0
+
+        for i in range(len(nums)):
+            rightSum=totalSum-nums[i]-prevSum
+            if prevSum==rightSum:
+                return i
+            prevSum+=nums[i]
+        return -1
+
+        
+        
+        
