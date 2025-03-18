5596ee36a6da0268f5fb0c91ace664d1f1fb2a91
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2025-03-19 00:53:42 +0600

    Time: 0 ms (100%), Space: 14.3 MB (62.42%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -7,14 +7,28 @@ from collections import deque
 #         self.right = right
 class Solution(object):
     def hasPathSum(self, root, targetSum):
-        def path(root,cur_sum):
+        """def path(root,cur_sum):
             if root is None:
                 return False
             cur_sum+=root.val
             if root.left is None and root.right is None:
                 return cur_sum==targetSum
             return (path(root.left,cur_sum) or path(root.right,cur_sum))
-        return path(root,0)
+        return path(root,0)"""
+        if root is None:
+            return False
+        stack=[(root,root.val)]
+
+        while stack:
+            node,cur_sum=stack.pop()
+            if node.left is None and node.right is None and cur_sum==targetSum:
+                return True
+            if node.left:
+                stack.append((node.left,cur_sum + node.left.val))
+            if node.right:
+                stack.append((node.right,cur_sum + node.right.val))
+        return False
+
        
         
       
