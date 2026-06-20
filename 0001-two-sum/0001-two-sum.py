4c83e07702aeab37f971f1d24189329541cc8ea2
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2026-06-20 14:32:18 +0600

    Time: 0 ms (100%), Space: 12.9 MB (81.87%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
+
+nums = [2, 7, 11, 15]
+target = 9
+
+sol = Solution()
+print(sol.twoSum(nums, target))
