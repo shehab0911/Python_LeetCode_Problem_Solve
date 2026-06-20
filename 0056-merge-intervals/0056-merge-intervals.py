5736fcb576d43b0aaa00a7ac1ce04315a5fadbd3
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2026-06-20 12:30:35 +0600

    Time: 8 ms (82.95%), Space: 15.9 MB (74.05%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals):
+        intervals.sort()
+        res = []
+
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+
+        return res
